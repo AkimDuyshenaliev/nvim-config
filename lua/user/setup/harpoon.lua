3777426bb28d90feaa209f6a5972525d244768ba
AkimDuyshenaliev <akim.duyshenaliev@gmail.com>
2024-05-24 20:19:26 +0300

    Some small harpoon setup refactoring

--- lua/user/setup/harpoon.lua
+++ lua/user/setup/harpoon.lua
@@ -9,13 +9,14 @@ harpoon:setup({
 })
 -- REQUIRED
 
-vim.keymap.set("n", "<A-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end) -- open quick meny
-vim.keymap.set("n", "<A-m>", function() harpoon:list():add() end) -- add buffer to default list
+-- M == Alt
+vim.keymap.set("n", "<M-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end) -- open quick meny
+vim.keymap.set("n", "<M-m>", function() harpoon:list():add() end) -- add buffer to default list
 
 for i=1, 9 do
-    vim.keymap.set("n", string.format("<A-%s>", i), function() harpoon:list():select(i) end) -- select n'th element from default list
+    vim.keymap.set("n", string.format("<M-%s>", i), function() harpoon:list():select(i) end) -- select n'th element from default list
 end
 
 -- Toggle previous & next buffers stored within Harpoon list
--- vim.keymap.set("n", "<A-S-P>", function() harpoon:list():prev() end)
--- vim.keymap.set("n", "<A-S-N>", function() harpoon:list():next() end)
+-- vim.keymap.set("n", "<M-S-P>", function() harpoon:list():prev() end)
+-- vim.keymap.set("n", "<M-S-N>", function() harpoon:list():next() end)
